b4343158b5419b6d8097f32f22f0df301f6d3829
copy90471 <copy90471@gmail.com>
2022-06-24 21:31:42 -0400

    Creating new app named model

---
+++ model/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+from model.models.Aeropuerto import Aeropuerto
+from model.models.Cliente import Cliente
+from model.models.Plazas import Plazas
+from model.models.Nave import Nave
+from model.models.Vuelo import Vuelo
+from model.models.Arribo import Arribo
+from model.models.Instalacion import Instalacion
+from model.models.Servicio import Servicio
+from model.models.Valoracion import Valoracion
+from model.models.Reparacion import Reparacion
+from model.models.ReparaNave import ReparaNave
+from model.models.ReparacionesDependientes import ReparacionesDependientes
+
+admin.site.register(Aeropuerto)
+admin.site.register(Cliente)
+admin.site.register(Plazas)
+admin.site.register(Nave)
+admin.site.register(Vuelo)
+admin.site.register(Arribo)
+admin.site.register(Instalacion)
+admin.site.register(Servicio)
+admin.site.register(Valoracion)
+admin.site.register(Reparacion)
+admin.site.register(ReparaNave)
+admin.site.register(ReparacionesDependientes)
---
+++ model/models/Reparacion.py
@@ -0,0 +1,39 @@
+from django.db import models
+from .Servicio import Servicio
+class Reparacion(models.Model):
+    # El id de esta tabla se oculta, ya hay llave primaria
+    # La Referencia a Servicio se oculta ya hay referencia por llave compuesta
+    Id_S = models.ForeignKey(Servicio,verbose_name="ID Servicio",null=False,on_delete=models.CASCADE)
+    Id_I = models.IntegerField(verbose_name="ID Instalacion",null=False)
+    Codigo = models.IntegerField(verbose_name="Codigo",null=False)
+    Tipo = models.CharField(verbose_name="Tipo",null=False,max_length=40)
+    class Meta:
+        constraints = [models.UniqueConstraint(fields=['Id_I','Codigo','Tipo'],name='Id de Reparacion')]
+
+    def __str__(self):
+        texto = "{0}"
+        return texto.format(self.id)
+    def __iter__(self):
+        for field_name in self._meta.fields:
+            if field_name.verbose_name == "ID": continue
+            if field_name.verbose_name == "ID Servicio":
+                value1=getattr(self,field_name.attname,None)
+                servicio = Servicio.objects.get(id=value1)
+                continue
+            value=getattr(self,field_name.attname,None)
+            if field_name.verbose_name == "ID Instalacion": 
+                self.Id_I = str(servicio.Id_I)
+                self.save()
+                value = self.Id_I
+            if field_name.verbose_name == "Codigo": 
+                self.Codigo = servicio.Codigo
+                self.save()
+                value = self.Codigo
+            yield value
+    def campos(self):
+        output=[]
+        for field_name in self._meta.fields:
+            if field_name.verbose_name == "ID": continue
+            if field_name.verbose_name == "ID Servicio": continue
+            output.append(field_name.verbose_name)
+        return output  
